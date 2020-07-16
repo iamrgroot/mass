7fa8d1628ccde3b01001dc1316bfe1ff7c8d2463
Robert Groot <rorogro@gmail.com>
2020-07-16 23:19:51 +0200

    PHP CS fix

--- app/Traits/FormatBytes.php
+++ app/Traits/FormatBytes.php
@@ -6,23 +6,23 @@ trait FormatBytes
 {
     public function formatBytes(?int $number): string
     {
-        if ($number === null) {
+        if (null === $number) {
             return '';
         }
-    
+
         $negative = $number < 0;
-        $units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    
+        $units    = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
         if ($negative) {
             $number = -$number;
         }
-    
+
         if ($number < 1) {
             return ($negative ? '-' : '') . $number . ' B';
         }
 
         $exponent = min(floor(log($number) / log(1000)), count($units) - 1);
-        $number = round(($number / pow(1000, $exponent)), 2);
+        $number   = round(($number / pow(1000, $exponent)), 2);
 
         return ($negative ? '-' : '') . "{$number} {$units[$exponent]}";
     }
