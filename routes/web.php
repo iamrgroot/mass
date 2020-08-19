c1f4cbf14d959a3afb59cc7d8f768e4586a3e6d4
IAmRGroot <rorogro@gmail.com>
2020-08-19 14:58:30 +0000

    PHP CS Fix

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::domain('home.' . config('app.host'))->group(function () {
-    Route::middleware('guest')->group(static function () { 
+    Route::middleware('guest')->group(static function () {
         Route::get('/login', 'Auth\LoginController@page')->name('login');
         Route::post('/login', 'Auth\LoginController@login')->name('login');
     });
