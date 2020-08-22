b35be62899005e68ef9f920171332123358a5d59
IAmRGroot <rorogro@gmail.com>
2020-08-22 13:20:45 +0000

    Removed subdomain hardcoding

--- routes/async.php
+++ routes/async.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::domain('home.' . config('app.host'))->group(function () {
+Route::domain(config('app.host'))->group(function () {
     Route::middleware('auth')->group(static function () {
         Route::prefix('movies')->group(static function () {
             Route::get('', 'Media\MovieController@movies');
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::domain('home.' . config('app.host'))->group(function () {
+Route::domain(config('app.host'))->group(function () {
     Route::middleware('guest')->group(static function () {
         Route::get('/login', 'Auth\LoginController@page')->name('login');
         Route::post('/login', 'Auth\LoginController@login')->name('login');
